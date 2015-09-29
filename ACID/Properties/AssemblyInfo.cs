c79687db2566decb03a311b41d828edc84ff8d77
Islam Wahdan <iislam27@hotmail.com>
2015-09-29 23:46:36 +0200

    Minor Change to project Description

--- ACID/Properties/AssemblyInfo.cs
+++ ACID/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ACID")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("POS System")]
+[assembly: AssemblyConfiguration("V01.01")]
+[assembly: AssemblyCompany("Islam Wahdan")]
 [assembly: AssemblyProduct("ACID")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
